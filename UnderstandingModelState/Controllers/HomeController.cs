a5fdef738cd86b18fb25e2a6544d32ca137c4512
Will 保哥 <doggy.huang@gmail.com>
2016-03-14 18:22:19 +0800

    實作 [HttpPost] Login 頁面的程式邏輯

--- UnderstandingModelState/Controllers/HomeController.cs
+++ UnderstandingModelState/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using UnderstandingModelState.Models;
 
 namespace UnderstandingModelState.Controllers
@@ -36,7 +37,24 @@ namespace UnderstandingModelState.Controllers
         [HttpPost]
         public ActionResult Login(LoginViewModel login)
         {
+            if (ModelState.IsValid && LoginCheck(login))
+            {
+                // 輸入驗證成功，所有欄位有效
+                FormsAuthentication
+                    .RedirectFromLoginPage(login.Email, false);
+
+                return RedirectToAction("Index");
+            }
+
             return View(login);
+        }
+
+        private bool LoginCheck(LoginViewModel login)
+        {
+            return (
+                login.Email == "test@example.com" &&
+                login.Password == "123"
+                );
         }
     }
 }
